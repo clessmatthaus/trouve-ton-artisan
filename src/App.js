838ee3a2bbfc1e4a222b0634840567739b64fca3
ClessMatthaus <cless91@live.fr>
2024-01-06 22:08:12 +0100

    add footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import {Routes, Route, useParams} from 'react-router-dom';
+import {BrowserRouter}  from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Header />
-    </div>
+      <Footer />
+    </div> 
   );
 }
 
